358b374fd566cceccc9beb8eb2c1b06caef228eb
brickwall <sk292207@gmail.com>
2022-09-10 21:47:04 +0530

    Add play function and checkifValid function
    play function saves computer move and player's move.
    checkifValid function checks if move is valid.

--- playrockpaperscissors.js
+++ playrockpaperscissors.js
@@ -1,11 +1,41 @@
 
 rpsListing = ["Rock","Paper","Scissors"];        //rock paper scissors listing
 
-function computerMove(){
+function getComputer(){
 
     let indexOf_rpsListing = Math.floor(Math.random()*3);
-    console.log(rpsListing[indexOf_rpsListing]);
+    return rpsListing[indexOf_rpsListing];
 }
 
-for(let i=0;i<10;i++)
-  computerMove();
+function play(){
+
+    computerSelection = getComputer();
+    playerSelection = checkifValid();
+    
+    console.log(computerSelection);
+    console.log(playerSelection);
+}
+
+function checkifValid(){
+  
+  /*Player enters his move in checkifValid function
+    If he enters valid move 
+    function returns the move if valid
+    else ask the player to enter his move again*/
+  
+  let flag = false; //to break the while loop
+  let inputMove;
+
+  while(flag===false){
+        
+    inputMove= prompt("Enter a valid move");
+    let capitalise_inputMove = inputMove.toUpperCase();
+
+    if(capitalise_inputMove==="ROCK"||capitalise_inputMove==="PAPER"||capitalise_inputMove==="SCISSORS"){  //capitalizing input and then comparing for valid move
+       flag = true;
+    }
+  }
+  return inputMove;
+}
+
+play(); //calling play function
